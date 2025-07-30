c1be98d3c2cc8f292f326cada6dfae73eb881359
Ismam Ahmed <130127017+Bishal2211@users.noreply.github.com>
2025-07-30 17:20:48 +0600

    red color  triangle

--- Cyan color window/src/main.cpp
+++ Cyan color window/src/main.cpp
@@ -6,7 +6,7 @@
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
-// settings
+// settings 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
